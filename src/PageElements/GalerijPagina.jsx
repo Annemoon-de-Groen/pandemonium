caa2fd2fa0b3a4089f1b84e1e0d967cb0ce4ce46
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-12-05 13:40:42 +0100

    Cast-pagina is nu daadwerkelijk mooi en de header heeft een schaduw want schaduwen maken alles mooier

--- src/PageElements/GalerijPagina.jsx
+++ src/PageElements/GalerijPagina.jsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom"
 import history from '../Assets/Data/history.json'
 import GreenThingy from "./GreenThingy"
+import { useState } from "react"
 
 function GalerijPagina(){
     const {voorstelling} = useParams()
     const item = history.history.find(x => x.thumbnail.link === voorstelling)
     const images = require.context('../Assets/Images/Spelling_bee/', true);
     const imageList = images.keys().map(image => images(image));
+    const [zoomedImage, setZoomedImage] = useState(null)
+
     return (
         <div>
             <GreenThingy text={item.thumbnail.naam} format={imageList[0]} /> 
@@ -23,13 +26,23 @@ function GalerijPagina(){
                 <div>
                     <div className="">
                         {imageList.map((image, index) => (
-                            <img key={index} src={image} alt={`image-${index}`} className="galerij_photo" />
+                            <img key={index} src={image} alt={`image-${index}`} className="galerij_photo" onClick={() => setZoomedImage(image)}/>
                         ))}
                     </div> 
                 </div>
             </div>
+
+            <div id="myModal" class="modal" style={{display:`${zoomedImage ? "block": "none"}`}}>
+
+                <span class="close" onClick={() => setZoomedImage(null)}>&times;</span>
+
+                <img class="modal-content" id="img01" alt="Spelling Bee" src={zoomedImage} />
+            </div>
         </div>
     )
+
 }
+
+
 
 export default GalerijPagina
