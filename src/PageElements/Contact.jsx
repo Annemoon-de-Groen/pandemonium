79b2c8d1d424a7203466423d2508aa98bd3ca2e4
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-12-13 12:36:09 +0100

    Custom domain

--- src/PageElements/Contact.jsx
+++ src/PageElements/Contact.jsx
@@ -12,31 +12,31 @@ const initialForm = {
 
 function Contact(){
 
-    const [formData, setFormData] = useState({...initialForm})2
-    function send_contact(e){
-        e.preventDefault()
-        console.log("Stuur email")
-        emailjs.send('service_qm5zcoh', 'template_t60l20l',
-        {
-          to_name: formData.firstName,
-          to_email: formData.email,
-          phone: formData.phone,
-          date: dateTime.toDateString(),
-          time: dateTime.toLocaleTimeString(),
-          subject: formData.message
-        }).then(
-          (response) => {
-            console.log('Succes!', response.status, response.text)
-          },
-          (error) => {
-            console.log('Error', error)
-          })
-        }
+    const [formData, setFormData] = useState({...initialForm})
+    // function send_contact(e){
+    //     e.preventDefault()
+    //     console.log("Stuur email")
+    //     emailjs.send('service_qm5zcoh', 'template_t60l20l',
+    //     {
+    //       to_name: formData.firstName,
+    //       to_email: formData.email,
+    //       phone: formData.phone,
+    //       date: dateTime.toDateString(),
+    //       time: dateTime.toLocaleTimeString(),
+    //       subject: formData.message
+    //     }).then(
+    //       (response) => {
+    //         console.log('Succes!', response.status, response.text)
+    //       },
+    //       (error) => {
+    //         console.log('Error', error)
+    //       })
+    //     }
 
 
-    emailjs.init({
-        publicKey: 'JKfJpovNNiVJINLud'
-    })
+    // emailjs.init({
+    //     publicKey: 'JKfJpovNNiVJINLud'
+    // })
 
     return (<>
         <GreenThingy text='Contact' format='../Assets/Images/cast_image.jpg'/>
@@ -70,7 +70,7 @@ function Contact(){
                     <label for="subject">Subject</label>
                     <textarea id="subject" name="subject" placeholder="Write something.." style={{"height":"200px"}}></textarea>
 
-                    <button className="submit" onClick={(e)=> send_contact(e)}>Verzenden</button>
+                    <button className="submit" onClick={(e)=> (e)}>Verzenden</button>
 
                 </form>
             </div>
